7c94cb0f2478beba74c9c2e6d0d13c91b068414e
Thomas_gllrme <thomas.guillerme.56@free.fr>
2024-05-14 13:34:57 +0200

    MANQUE ARDUINO.H

--- main/main.c
+++ main/main.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
-#include <Arduino.h>
 #include"main.h"
 
 void app_main(void)
 {
-    printf("Debug_V1\n"); // Print "Debug_V1" to the console   
+    printf("Debug_V2\n"); // Print "Debug_V1" to the console   
 
 
     
