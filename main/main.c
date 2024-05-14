de245eb103e3a2a62043bd593421d7c231bc7ad5
Thomas_gllrme <thomas.guillerme.56@free.fr>
2024-05-14 18:16:38 +0200

    Alarme Incendie

--- main/main.c
+++ main/main.c
@@ -6,7 +6,7 @@
 #include "freertos/FreeRTOS.h"
 
 int pwmChannel = 0; //Choisit le canal 0
-int frequence = 1000; //Fréquence PWM de 1 KHz
+int frequence = 440; //Fréquence PWM de 1 KHz
 int resolution = 8; // Résolution de 8 bits, 256 valeurs possibles
 int pwmPin = 23;
 
@@ -33,8 +33,21 @@ void app_main(void)
     printf("Debug_V13\n"); // Print "Debug_V13" to the console   
     ESP_ERROR_CHECK(ledc_channel_config(&pwm_channel)); //Configure le canal PWM
     ESP_ERROR_CHECK(ledc_timer_config(&pwm_config)); //Configure le timer PWM
+    ESP_ERROR_CHECK(ledc_stop(LEDC_LOW_SPEED_MODE, pwmChannel, 0)); //Arrête le PWM (0 = Ne pas attendre la fin du cycle actuel
+    //ESP_ERROR_CHECK(ledc_fade_func_install(0)); //Installe la fonction de fondu
 
-
-
-    ESP_ERROR_CHECK(ledc_fade_func_install(0)); //Installe la fonction de fondu
+   while (true)
+   {
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, pwmChannel)); //Démarre le PWM à 0
+    ESP_ERROR_CHECK(ledc_set_freq(pwm_channel.speed_mode, pwm_config.timer_num,440)); //Démarre le PWM à 0
+    vTaskDelay(400 / portTICK_PERIOD_MS); //Attend 1 seconde
+    ESP_ERROR_CHECK(ledc_stop(LEDC_LOW_SPEED_MODE, pwmChannel, 0)); //Arrête le PWM (0 = Ne pas attendre la fin du cycle actuel
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, pwmChannel)); //Démarre le PWM à 0
+    ESP_ERROR_CHECK(ledc_set_freq(pwm_channel.speed_mode, pwm_config.timer_num,550)); //Démarre le PWM à 0
+    vTaskDelay(100/ portTICK_PERIOD_MS); //Attend 1 seconde
+    ESP_ERROR_CHECK(ledc_stop(LEDC_LOW_SPEED_MODE, pwmChannel, 0)); //Arrête le PWM (0 = Ne pas attendre la fin du cycle actuel
+   }
+    
+   
+    
 }  
