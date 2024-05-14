8c6ab66e6dee96c9a5162a9068fd06f13ae6287e
Thomas_gllrme <thomas.guillerme.56@free.fr>
2024-05-14 13:22:29 +0200

    C6

--- main/main.c
+++ main/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+#include <arduino.h>
 void app_main(void)
 {
-    printf("Hello world!\n"); // Print "Hello world!" to the console    
+    printf("Debug_V1\n"); // Print "Debug_V1" to the console   
+
+
     
 }
